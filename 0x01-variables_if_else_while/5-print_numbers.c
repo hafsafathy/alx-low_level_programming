18e223bbc1120896f63d9e8f4602c295b82dea6a
hafsafathy <hafsafathy020@gmail.com>
2023-06-16 15:17:49 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	while (num < 10)
+	{
+		printf("%d", num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
