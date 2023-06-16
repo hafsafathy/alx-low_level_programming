bafc08c05e8c0c27666b8df4a817f5e5d9ec4ff4
hafsafathy <hafsafathy020@gmail.com>
2023-06-16 14:35:11 -0700

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 26; c++)
+	{
+		putchar(c + 97);
+	}
+	for (c = 0; c < 26; c++)
+        {
+                putchar(c + 65);
+        }
+	putchar('\n');
+	return (0);
+}
