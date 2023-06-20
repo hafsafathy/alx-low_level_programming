8fed06efecfdbe21bfbed6ac0c00ae5e8da5cb96
hafsafathy <hafsafathy020@gmail.com>
2023-06-20 11:16:36 -0700

    12. The World looks like a multiplication-table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -20,7 +20,7 @@ void print_times_table(int n)
 				{
 					_putchar(k + '0');
 				}
-			       	else if (k < 10 && j != 0)
+				else if (k < 10 && j != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
@@ -28,15 +28,14 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(k + '0');
 				}
-			       	else if (k >= 10 && k < 100)
+				else if (k >= 10 && k < 100)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
-				} 
-				else if (k >= 100)
+				} else if (k >= 100)
 				{
 					_putchar(',');
 					_putchar(' ');
@@ -48,5 +47,4 @@ void print_times_table(int n)
 			_putchar('\n');
 		}
 	}
-
 }
