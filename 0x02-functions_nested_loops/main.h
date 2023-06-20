f7200ff184badea891bb5c59c7ea9fc66d89f876
hafsafathy <hafsafathy020@gmail.com>
2023-06-20 09:09:38 -0700

    8. Iam federal agent Jack Bauer

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,5 +9,11 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int r);
 int print_last_digit(int r);
+void jack_bauer(void);
+void times_table(void);
+int add(int a, int b);
+void print_to_98(int n);
+void print_times_table(int n);
+void putformat(int);
 
 #endif
