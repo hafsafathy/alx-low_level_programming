f16514aec5c507718da5b43efca43b683fbd072a
hafsafathy <hafsafathy020@gmail.com>
2023-06-19 22:56:39 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Check description
+ * Description: It prints the word Holberton, followed by a new line.
+ * Return:  always  0(success) .
+ */
+int main(void)
+{
+	char input[8] = "_putchar";
+	int a;
+
+	for (a = 0; a < 8; a++)
+		_putchar(input[a]);
+	_putchar('\n');
+
+	return (0);
+}
