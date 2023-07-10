7c35ba747ac059cba9dab007611af7f3fe2b6ebe
hafsafathy <hafsafathy020@gmail.com>
2023-07-10 11:25:38 -0700

    3. task

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -27,6 +27,8 @@ int **alloc_grid(int width, int height)
 		ptr[i] = (int *)malloc(sizeof(int) * width);
 		if (ptr[i] == NULL)
 		{
+			for (i--; i >= 0; i--)
+				free(ptr[i]);
 			free(ptr);
 			return (NULL);
 		}
