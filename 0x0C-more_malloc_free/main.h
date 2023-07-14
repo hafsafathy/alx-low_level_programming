d3fdef5ac18f3438b7d1ad7d77b131b2e5c96177
hafsafathy <hafsafathy020@gmail.com>
2023-07-13 19:59:37 -0700

    well

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
+
+void _puts(char *str);
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
