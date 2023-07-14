0c7cecbe8a38714c18a2d8e0d5c7a988ddbb92bb
hafsafathy <hafsafathy020@gmail.com>
2023-07-13 17:32:16 -0700

    101. task

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,6 +2,27 @@
 #include <stdlib.h>
 
 /**
+ * checknum - determines length of the number
+ * and checks if number is in base 10.
+ * @argv: arguments vector.
+ * @n: row of the array.
+ *
+ * Return: length of the number.
+ */
+int checknum(char *argv[], int n)
+{
+	int ln;
+
+	for (ln = 0; argv[n][ln]; ln++)
+		if (!isdigit(argv[n][ln]))
+		{
+			printf("Error\n");
+			exit(98);
+		}
+
+	return (ln);
+}
+/**
  * main - program that multiplies two positive numbers.
  * @c: number of arguments.
  * @v: arguments.
@@ -12,6 +33,11 @@ int main(int c, char **v)
 	int num1, num2, mul;
 
 	if (c != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if else (num1 = checknum(v, 1), num2 = checknum(v, 2))
 	{
 		printf("Error\n");
 		exit(98);
