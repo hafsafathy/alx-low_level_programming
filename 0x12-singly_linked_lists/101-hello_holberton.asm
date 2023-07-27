a2e7aa9e367ed7178e1ad07a03c14de50263900e
hafsafathy <hafsafathy020@gmail.com>
2023-07-27 15:30:29 -0700

    101.task

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,10 +1,15 @@
-section	       .text
+	SECTION .data
+msg:	db "Hello, Holberton", 0
+fmt:	db "%s", 10, 0
+
+	SECTION .text
 	extern printf
 	global main
 main:
-	mov	edi, msg
-	mov	eax, 0
-	call	printf
+	mov esi, msg
+	mov edi, fmt
+	mov eax, 0
+	call printf
 
-section	       .data
-msg:	db "Hello, Holberton", 0
+	mov eax, 0
+	ret
