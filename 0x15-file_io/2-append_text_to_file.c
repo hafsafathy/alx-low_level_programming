ad05ffa9d7c304cf80493306fdedb9c1f79a02eb
hafsafathy <hafsafathy020@gmail.com>
2023-08-07 17:46:40 -0700

    2. task

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -2,7 +2,7 @@
 
 /**
  * append_text_to_file - function that appends text at the end of a file
- * @filename: is the name of the file. 
+ * @filename: is the name of the file.
  * @text_content:  is the NULL terminated string to add at the end of the file.
  * Return: 1 on success and -1 on failure.
  */
